ef54d81075ece8c04e51a73eed5bbf4ce3786ba9
aaronwinter <aaron.ounn@gmail.com>
2018-04-10 14:59:12 +0200

    arc/test: Remove obsolete tests

--- arc_test.go
+++ arc_test.go
@@ -3,7 +3,6 @@ package gcache
 import (
 	"fmt"
 	"testing"
-	"time"
 )
 
 func buildARCache(size int) Cache {
@@ -16,15 +15,6 @@ func buildARCache(size int) Cache {
 func buildLoadingARCache(size int) Cache {
 	return New(size).
 		ARC().
-		LoaderFunc(loader).
-		EvictedFunc(evictedFuncForARC).
-		Build()
-}
-
-func buildLoadingARCacheWithExpiration(size int, ep time.Duration) Cache {
-	return New(size).
-		ARC().
-		Expiration(ep).
 		LoaderFunc(loader).
 		EvictedFunc(evictedFuncForARC).
 		Build()
@@ -45,69 +35,4 @@ func TestLoadingARCGet(t *testing.T) {
 	size := 1000
 	numbers := 1000
 	testGetCache(t, buildLoadingARCache(size), numbers)
-}
-
-func TestARCLength(t *testing.T) {
-	gc := buildLoadingARCacheWithExpiration(2, time.Millisecond)
-	gc.Get("test1")
-	gc.Get("test2")
-	gc.Get("test3")
-	length := gc.Len()
-	expectedLength := 2
-	if length != expectedLength {
-		t.Errorf("Expected length is %v, not %v", expectedLength, length)
-	}
-	time.Sleep(time.Millisecond)
-	gc.Get("test4")
-	length = gc.Len()
-	expectedLength = 1
-	if length != expectedLength {
-		t.Errorf("Expected length is %v, not %v", expectedLength, length)
-	}
-}
-
-func TestARCEvictItem(t *testing.T) {
-	cacheSize := 10
-	numbers := cacheSize + 1
-	gc := buildLoadingARCache(cacheSize)
-
-	for i := 0; i < numbers; i++ {
-		_, err := gc.Get(fmt.Sprintf("Key-%d", i))
-		if err != nil {
-			t.Errorf("Unexpected error: %v", err)
-		}
-	}
-}
-
-func TestARCPurgeCache(t *testing.T) {
-	cacheSize := 10
-	purgeCount := 0
-	gc := New(cacheSize).
-		ARC().
-		LoaderFunc(loader).
-		PurgeVisitorFunc(func(k, v interface{}) {
-			purgeCount++
-		}).
-		Build()
-
-	for i := 0; i < cacheSize; i++ {
-		_, err := gc.Get(fmt.Sprintf("Key-%d", i))
-		if err != nil {
-			t.Errorf("Unexpected error: %v", err)
-		}
-	}
-
-	gc.Purge()
-
-	if purgeCount != cacheSize {
-		t.Errorf("failed to purge everything")
-	}
-}
-
-func TestARCGetIFPresent(t *testing.T) {
-	testGetIFPresent(t, TYPE_ARC)
-}
-
-func TestARCGetALL(t *testing.T) {
-	testGetALL(t, TYPE_ARC)
 }
